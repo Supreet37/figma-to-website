137689b6cb1375adb67da02250e958691cf46121
supreet mohapatra <supreetmohapatra06@gmail.com>
2026-06-10 21:10:57 +0530

    Supermarket website

---
+++ figma5/script.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header");
+const navbar = document.getElementById("navbar");
+
+navbar.addEventListener("mouseenter", () => {
+  header.style.display = "flex";
+});
+
+
+
+
+
